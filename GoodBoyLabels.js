377a52403c056442d3589d05980774ba95196f11
Good Boy Ninja <66829812+GoodBoyNinja@users.noreply.github.com>
2022-08-27 22:41:08 +0200

    MacOS Fix, now gets label names as well
    Path to macOS prefs file was broken, and is now fixed.
    The code has been formatted to be more readable and easier to understand.
    In addition, the function now returns an object of two arrays, "names" and "colors".
    These arrays have corresponding indices, and both are sorted by the ID of the label, as written in preferences.

--- GoodBoyLabels.js
+++ GoodBoyLabels.js
@@ -1,98 +1,101 @@
 // main function
-function getLabelColors () {
+function getLabelsInfo() {
+
+    const result = {
+        names: Array(17).fill("#000000"),
+        colors: Array(17).fill("Black"),
+    };
 
     // requires nodejs to read the preference file
     const fs = cep_node.require('fs');
     const path = cep_node.require('path');
     const cs = new CSInterface();
-    
+
     // check if macOS or windows
-    const osName = GEP.cs.getOSInformation().toLowerCase();
-    const isMacOS = osName.indexOf('windows') ? false : true;   
+    const osName = cs.getOSInformation().toLowerCase();
+    const isMacOS = osName.indexOf('windows') > -1 ? false : true;
 
-    // get the AE version, down to 1 decimal place (that's what the folder names are using)
-    var version = cs.getHostEnvironment().appVersion;
+    let version = cs.getHostEnvironment().appVersion;
     version = version.split(".")[0] + "." + version.split(".")[1];
+    let userDataFolder = cs.getSystemPath(SystemPath.USER_DATA);
+    let prefsFolder = isMacOS ? path.join(path.dirname(userDataFolder), "Preferences", "Adobe", "After Effects", version) : path.join(userDataFolder, "Preferences", "Adobe", "After Effects", version);
+    let prefsFile = path.join(prefsFolder, `Adobe After Effects ${version} Prefs-indep-general.txt`);
 
-    // finding the preferences folder
-    var userDataFolder = cs.getSystemPath(SystemPath.USER_DATA);
-    var prefsFolder = isMacOS ? path.join(userDataFolder, "Adobe", "After Effects", version) : path.join(userDataFolder, "Preferences", "Adobe", "After Effects", version)(userDataFolder, "Preferences");
-    var prefsFile = path.join(prefsFolder, `Adobe After Effects ${version} Prefs-indep-general.txt`);
-
-    var prefsFolderExists = fs.existsSync(prefsFolder);
-    var prefsFileExists = fs.existsSync(prefsFile);
-
-    if (prefsFolderExists && prefsFileExists) {
-        // read the file using nodejs
-        var prefs = fs.readFileSync(prefsFile, "utf8");
-
-        // get only the label colors section
-        const start = `"Label Color ID 2 # 1"`;
-        const startIndex = prefs.indexOf(start);
-        const split = prefs.substring(startIndex, prefs.length);
-        const section = split.substring(0, split.indexOf('[') - 1);
-
-        // split the section into lines
-        var labelSettingsLines = section.split('\n');
-        var labelColors = [];
-
-        // loop through each line and...
-        for (var i = 0; i < labelSettingsLines.length; i++) {
-            const crnt = labelSettingsLines[i];
-
-            // get the id
-            var id = crnt.substring(crnt.indexOf('#') + 1);
-            id = id.substring(0, id.indexOf('"'));
-            id = Number(id);
-
-            // get the raw color, as displayed in AE
-            var rawColor = crnt.substring(crnt.indexOf('=') + 1).trim();
-
-            // raw color is weird. It's half HEX and half ASCII.
-            // the part that's ASCII is inside quotation marks.
-            // Let's convert it to hex
-            
-            // (the following part has been contributed by @Eyalco1, the above comments may be outdated)
-            const splits = rawColor.split('"');
-            const filteredIndexes = splits.map((s, i) => {
-            if (i % 2 !== 0) return i
-                }).filter(i => i);
-                splits.forEach((s, i) => {
-                    if (filteredIndexes.includes(i)) {
-                        splits[i] = ascii2hex(splits[i])
-                    }
-            });
-            
-             // replace it and ommit the first two letters
-            const hexColor = splits.join('').substring(2); // "4AA44C"
-            
-
-            // log it if you want to see what's up:
-            // console.log(hexColor);
-
-            // push it into the array, with the id as the key
-            labelColors[id] = hexColor;
-        }
-
-
-        return labelColors;
+    if (!fs.existsSync(prefsFolder) || !fs.existsSync(prefsFile)) {
+        console.warn("Failed to get label colors? Returning fallback result.");
+        return result;
     }
 
-    return null;
+    // read the file using nodejs and find the right sections for both label colors and label names
+    let prefsTxt = fs.readFileSync(prefsFile, "utf8");
+    let hexSection = prefsTxt.substring(prefsTxt.indexOf(`"Label Color ID 2 # 1"`), prefsTxt.length);
+    hexSection = hexSection.substring(0, hexSection.indexOf('[') - 1);
+    let namesSection = prefsTxt.substring(prefsTxt.indexOf(`"Label Preference Text Section 7"`), prefsTxt.length);
+    namesSection = namesSection.substring(namesSection.indexOf('['), namesSection.indexOf(']') + 1);
 
+
+    // the names section and the hex section are really similar but have small differences. Let's parst them both together and make up for the variations as we go.
+    let raw = Array(17).fill().map(i => ({}));
+    [hexSection, namesSection].forEach(section => {
+        // break to lines
+        let isHexSection = section === hexSection;
+        let linesSeperator = isHexSection ? '"Label Color ID ' : '"Label Text ID ';
+        let lines = section.split(linesSeperator);
+        let key = isHexSection ? "value" : "name";
+
+        // read from each line
+        lines.forEach(line => {
+            let hashIndex = line.indexOf('#');
+            let id = Number(line.substring(hashIndex + 1, line.indexOf('"', hashIndex + 1)));
+            let value = line.substring(line.indexOf("= ") + 2, line.length).trim();
+
+            // for names, we also want to yeet the quotes
+            if (!isHexSection) {
+                value = value.replace(/['"]+/g, '');
+            }
+
+            raw[id][key] = value; // register value or name in the raw array
+        });
+    });
+
+    // fix them colors
+    for (let i = 0; i < raw.length; i++) {
+        let crntInfoSet = raw[i];
+        let id = i;
+        let rawName = crntInfoSet["name"];
+        let rawColor = crntInfoSet["value"];
+
+        // raw color is weird. It's half HEX and half ASCII.
+        // the part that's ASCII is inside quotation marks.
+        // Let's convert it to hex
+
+        const splits = rawColor.split('"');
+        const filteredIndexes = splits.map((s, i) => {
+            if (i % 2 !== 0) return i;
+        }).filter(i => i);
+        splits.forEach((s, i) => {
+            if (filteredIndexes.includes(i)) {
+                splits[i] = ascii2hex(splits[i]);
+            }
+        });
+        const hexColor = splits.join('').substring(2);
+
+        result.names[id] = rawName;
+        result.colors[id] = hexColor;
+
+    }
+    return result;
 
 };
 
 
 // util function to convert ascii to hex
 function ascii2hex(str) {
-  var arr = [];
+    var arr = [];
 
-  for (var i = 0, l = str.length; i < l; i++) {
-      var hex = Number(str.charCodeAt(i)).toString(16);
-      arr.push(hex);
-  }
-  return arr.join('');
+    for (var i = 0, l = str.length; i < l; i++) {
+        var hex = Number(str.charCodeAt(i)).toString(16);
+        arr.push(hex);
+    }
+    return arr.join('');
 }
-
-
